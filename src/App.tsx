8aaeb16a3c1080bbd19f0afcde1809e460bc00c4
Matt <mathews123456@gmail.com>
2025-10-27 01:14:48 +0100

    analytics dropped

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const queryClient = new QueryClient();
@@ -27,7 +26,6 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <AnimatePresence mode="wait">
-            <Analytics />
             <SpeedInsights />
             <Routes>
               <Route path="/" element={<Index />} />
